7a752e5e63bf13e5434a3a5c3fd011ec4f5761ab
Bri <34875062+Monkatraz@users.noreply.github.com>
2022-03-17 01:40:15 -0600

    document TarnationLanguage

--- src/language.ts
+++ src/language.ts
@@ -13,26 +13,84 @@ import { ParserFactory } from "./parser"
 import type { ParserConfiguration, TarnationLanguageDefinition } from "./types"
 import { makeTopNode, removeUndefined } from "./util"
 
+/**
+ * Tarnation language. Use the `load` method to get the extension needed to
+ * load the language into CodeMirror. If you need a `LanguageDescription`,
+ * the `description` property will hold one.
+ */
 export class TarnationLanguage {
+  /** CodeMirror language data. */
   declare languageData: Record<string, any>
+
+  /** The grammar definition (or a function that returns one). */
   declare grammarData: DF.Grammar | (() => DF.Grammar)
+
+  /** Extra configuration for the parser. */
   declare configure: ParserConfiguration
+
+  /** List of extensions to load with the language. */
   declare extensions: Extension[]
+
+  /** `LanguageDescription` instance for the language. */
   declare description: LanguageDescription
+
+  /**
+   * Languages that are supported for nesting. Can also be a facet that
+   * provides the list.
+   */
   declare nestLanguages: LanguageDescription[] | Facet<LanguageDescription>
 
   // only shows up after loading
 
+  /** Fully resolved grammar instance. Requires the language to have been loaded. */
   declare grammar?: Grammar
+
+  /**
+   * The CodeMirror top node for the language. Requires the language to
+   * have been loaded.
+   */
   declare top?: NodeType
+
+  /**
+   * All of the node types used by the language. Requires the language to
+   * have been loaded.
+   */
   declare nodeTypes?: NodeType[]
+
+  /**
+   * CodeMirror `NodeSet` for the language. Requires the language to have
+   * been loaded.
+   */
   declare nodeSet?: NodeSet
+
+  /**
+   * The `NodeProp` that points to the `ChunkBuffer` data. Will be assigned
+   * to the top node of the language. Requires the language to have been loaded.
+   */
   declare stateProp?: NodeProp<ChunkBuffer>
+
+  /**
+   * Autocompleter instance. Requires the language to have been loaded, but
+   * will only be there if there are any autocompletion handlers.
+   */
   declare autocompleter?: Autocompleter
+
+  /**
+   * `LanguageSupport` instance for the language. Requires the language to
+   * have been loaded.
+   */
   declare support?: LanguageSupport
+
+  /**
+   * CodeMirror `language` instance for the language. Requires the language
+   * to have been loaded.
+   */
   declare language?: Language
 
+  /** Will be true if the langauge has been loaded once. */
   loaded = false
+
+  /** Will be set to the number of milliseconds the last parse time took. */
   performance = 0
 
   constructor({
